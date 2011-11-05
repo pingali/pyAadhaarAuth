9ae9662857b6b1b85645194a5926797d2330cfe8
Venkata Pingali <pingali@gmail.com>
2011-11-05 13:14:07 +0530

    Added lookup for errors

--- AadhaarAuth/response.py
+++ AadhaarAuth/response.py
@@ -31,6 +31,8 @@ from config import Config
 import traceback 
 import base64 
 from datetime import * 
+import hashlib 
+import binascii 
 
 __author__ = "Venkata Pingali"
 __copyright__ = "Copyright 2011,Venkata Pingali and TCS" 
@@ -55,7 +57,8 @@ class AuthResponse():
     python-based. This interface supports only v1.5 and public AuAs 
     """
 
-    def __init__(self, cfg=None, ret="n", txn="", 
+
+    def __init__(self, cfg=None, uid=None, ret="n", txn="", 
                  err=-1, info="", ts=datetime.utcnow(), 
                  code=-1):
         
@@ -68,6 +71,163 @@ class AuthResponse():
         self._ts = ts
         self._code = code 
         self._err = err 
+        self._uid = uid 
+
+    def lookup_error(self): 
+        
+        # 
+        errors = { 
+            '100': "'Pi' (basic) attributes of demographic data did not match.",
+            '200': "'Pa' (address) attributes of demographic data did not match",
+            '300': "Biometric data did not match",
+            '500': "Invalid encryption",
+            '510': "Invalid Auth XML format",
+            '511': "Invalid PID XML format",
+            '520': "Invalid device",
+            '530': "Invalid authenticator code",
+            '540': "Invalid version",
+            '550': "Invalid 'Uses' element attributes",
+            '561': "Request expired ('Pid->ts' value is older than N hours where N is a configured threshold in authentication server)",
+            '562': "Timestamp value is future time (value specified 'Pid->ts' is ahead of authentication server time beyond acceptable threshold)",
+            '563': "Duplicate request (this error occurs when exactly same authentication request was re-sent by AUA)",
+            '564': "HMAC Validation failed",
+            '565': "License key has expired",
+            '566': "Invalid license key",
+            '567': "Invalid input (this error occurs when some unsupported characters were found in Indian language values, 'lname' or 'lav')",
+            '568': "Unsupported Language",
+            '569': "Digital signature verification failed (this means that authentication request XML was modified after it was signed)",
+            '570': "Invalid key info in digital signature (this means that certificate, used for signing the authentication request is not valid - it is either expired, or does not belong to the AUA or is not created by a well-known (Certification Authority)",
+            '571': "PIN Requires reset (this error will be returned if resident is using the default PIN which needs to be reset before usage)",
+            '572': "Invalid biometric position (This error is returned if biometric position value - 'pos' attribute in 'Bio' element - is not applicable for a given biometric type - 'type' attribute in 'Bio' element.)",
+            '573': "Pi usage not allowed as per license",
+            '574': "Pa usage not allowed as per license",
+            '575': "Pfa usage not allowed as per license",
+            '576': "FMR usage not allowed as per license",
+            '577': "FIR usage not allowed as per license",
+            '578': "IIR usage not allowed as per license",
+            '579': "OTP usage not allowed as per license",
+            '580': "PIN usage not allowed as per license",
+            '581': "Fuzzy matching usage not allowed as per license",
+            '582': "Local language usage not allowed as per license",
+            '700': "Invalid demographic data",
+            '710': "Missing 'Pi' data as specified in 'Uses'",
+            '720': "Missing 'Pa' data as specified in 'Uses'",
+            '721': "Missing 'Pfa' data as specified in 'Uses'",
+            '730': "Missing PIN data as specified in 'Uses'",
+            '740': "Missing OTP data as specified in 'Uses'",
+            '800': "Invalid biometric data",
+            '810': "Missing biometric data as specified in 'Uses'",
+            '811': "Missing biometric data in CIDR for the given Aadhaar number",
+            '820': "Missing or empty value for 'bt' attribute in 'Uses' element",
+            '821': "Invalid value in the 'bt' attribute of 'Uses' element",
+            '901': "No authentication data found in the request (this corresponds to a scenario wherein none of the auth data - Demo, Pv, or Bios - is present)",
+            '902': "Invalid 'dob' value in the 'Pi' element (this corresponds to a scenarios wherein 'dob' attribute is not of the format 'YYYY' or 'YYYY-MM-DD', or the age of resident is not in valid range)",
+            '910': "Invalid 'mv' value in the 'Pi' element",
+            '911': "Invalid 'mv' value in the 'Pfa' element",
+            '912': "Invalid 'ms' value",
+            '913': "Both 'Pa' and 'Pfa' are present in the authentication request (Pa and Pfa are mutually exclusive)",
+            '930': "Technical error that are internal to authentication server",
+            '931': "Technical error that are internal to authentication server",
+            '932': "Technical error that are internal to authentication server",
+            '933': "Technical error that are internal to authentication server",
+            '934': "Technical error that are internal to authentication server",
+            '935': "Technical error that are internal to authentication server",
+            '936': "Technical error that are internal to authentication server",
+            '937': "Technical error that are internal to authentication server",
+            '938': "Technical error that are internal to authentication server",
+            '939': "Technical error that are internal to authentication server",
+            '940': "Unauthorized ASA channel",
+            '941': "Unspecified ASA channel",
+            '980': "Unsupported option",
+            '999': "Unknown error",
+            }
+        
+        err = self._response['_err']
+        res = errors[err]
+        print "Error lookup for %s: %s " % (err, res)
+        return res
+        
+    def lookup_usage_bits(self, what=None):
+        
+        def pos(digit, position):
+            return ((digit * 4) - position)
+
+        usage_positions = { 
+            
+            # 1st hexadecimal digit: Bit 3-0: Version number of
+            # encoding. It will be hexadecimal "1" (binary: 0001, for
+            # encoding specified in this document.
+            
+            'ver': pos(1,0),
+            
+            # 2nd hexadecimal digit
+            'Pi->name':pos(2,3), 
+            'Pi->lname':pos(2,2),
+            'Pi->gender':pos(2,1),
+            'Pi->dob':pos(2,0),
+            
+            # 3rd hexadecimal digit
+            'Pi->phone':pos(3,3),
+            'Pi->email':pos(3,2),
+            'Pi->age':pos(3,2),
+            'Pa->co':pos(3,1),
+            
+            # 4th hexadecimal digit
+            'Pa->house':pos(4,3),
+            'Pa->street':pos(4,2),
+            'Pa->lm':pos(4,1),
+            'Pa->loc':pos(4,0),
+            
+            # 5th hexadecimal digit
+            'Pa->vtc':pos(5,3),
+            'Pa->dist':pos(5,2),
+            'Pa->state':pos(5,1),
+            'Pa->pc':pos(5,0),
+            
+            # 6th hexadecimal digit
+            'Pfa->av':pos(6,3),
+            'Pfa->lav':pos(6,2),
+            'FMR':pos(6,1),
+            'FIR':pos(6,0), 
+            
+            # 7th hexadecimal digit
+            'IIR':pos(7,3),
+            'Pv->pin':pos(7,2),
+            'Pv->otp':pos(7,1),
+            'Tkn':pos(7,0),
+            
+            # 8th hexadecimal digit
+            'Pa->po':pos(8,3),
+            'Pa->subdist':pos(8,2),
+            'Pi->dobt':pos(8,1),
+            
+            # Bit 0: Unused
+            # 9th to 12th Hexadecimal digits
+            # Currently unused. Will have value 0.
+            }
+
+        if (what != None): 
+            try: 
+                position = usage_positions[what]
+                flag = self._response['_usage_data'][position-1]
+                print "Looked up flag %s - bit pos %d with result %s " \
+                    % (what, position, flag)
+            except: 
+                raise Exception("Unknown flag to lookup in usage data") 
+
+            return flag
+        else:
+            # return all the flags that are set
+            res = []
+            all_usages = usage_positions.keys() 
+            for what in all_usages:
+                position = usage_positions[what]
+                flag = self._response['_usage_data'][position-1]
+                #print "Looked up flag %s - bit pos %d with result %s " \
+                #    % (what, position, flag)
+                if (flag == "1"):
+                    res = res + [what]
+            return res 
 
     def validate(self): 
         
@@ -79,6 +239,7 @@ class AuthResponse():
         #    raise Exception("Response too old?") 
 
         return True 
+
 
     def set_err(self, value): 
         self._err = value
@@ -129,11 +290,39 @@ class AuthResponse():
         doc = etree.ElementTree(root) 
         return ("<?xml version=\"1.0\"?>\n%s" %(etree.tostring(doc, pretty_print=True)))
 
-    #def load(self, xmlfile, xsdfile):
-    #    xml_text = file(xmlfile).read() 
-    #    o = self.xsd_check(xml_text, xsdfile) 
-    
-    
+    def load_string(self, xml):
+
+        try: 
+            obj = objectify.fromstring(xml)
+        except:
+            raise Exception("Could not parse the XML response") 
+        
+        #print type(obj)        
+        #print obj.get('info')
+
+        self._response = { 
+            '_info':  obj.get('info'), 
+            '_err': obj.get('err'), 
+            '_code': obj.get('code'),
+            '_ret': obj.get('ret'),
+            '_ts': obj.get('ts'),
+            '_txn': obj.get('txn')
+            }
+        self._response['_ver'] = self._response['_info'][0:2],
+        self._response['_uid_hash'] = self._response['_info'][2:66],
+        self._response['_pidxml_demographics_hash'] = self._response['_info'][66:130],
+
+        # => Turn the hex info into a flag 
+        def hextobin(s):
+            bin_data = ""
+            for i in range(0,len(s)):
+                h=s[i]
+                bin_data = bin_data + bin(int(h, 16))[2:].zfill(4)
+            return bin_data 
+        usage_data =self._response['_info'][130:142]
+        self._response['_usage_data'] = hextobin(usage_data)
+        return 
+        
 if __name__ == '__main__':
     assert(sys.argv)
     if len(sys.argv) < 2:
@@ -153,16 +342,25 @@ response: {
 """    
     cfg = Config(sys.argv[1]) 
     if (cfg.response.command == "generate"):
-        x = AuthResponse(cfg, err=100, ts=datetime.utcnow())
-        x.validate() 
-        xml = x.tostring() 
+        response = AuthResponse(cfg, err=100, ts=datetime.utcnow())
+        response.validate() 
+        xml = response.tostring() 
         print s 
-        x.xsd_check(xml, cfg.common.response_xsd)
+        response.xsd_check(xml, cfg.common.response_xsd)
     elif (cfg.response.command == "validate"): 
+        response = AuthResponse(cfg=cfg)
         test_xml = file(cfg.response.xml).read() 
         print "Validating this incoming XML" 
         print test_xml
-        x.xsd_check(test_xml, cfg.common.response_xsd) 
+
+        #sig = AuthRequestSignature() 
+        #sig.verify_file(cfg.response.xml, cfg.common.uid_cert_path)
+
+        #response.xsd_check(test_xml, cfg.common.response_xsd) 
+        response.load_string(test_xml) 
+        response.lookup_error()
+        print "Flags that are set: ", response.lookup_usage_bits()
+        
     else:
         print "Unknown command %s " % cfg.response.command
         sys.exit(1) 
