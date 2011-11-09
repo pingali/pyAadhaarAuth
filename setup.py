f8e672102832d6c3eb71d6f1bc469fa78d966ad9
Venkata Pingali <pingali@gmail.com>
2011-11-09 13:10:51 +0530

    Flaky but roughly working setup. Need to test on ubuntu.

--- setup.py
+++ setup.py
@@ -2,15 +2,31 @@
 
 from distutils.core import setup
 
+classifiers = [    
+    'Development Status :: 2 - Pre-Alpha',    
+    'Intended Audience :: Developers',    
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: POSIX',    
+    'Programming Language :: Python',    
+    'Topic :: System :: Systems Administration :: Authentication/Directory']
+
+
 setup(name='pyAadhaarAuth',
       version='0.1.0',
       description='UIDAI Authentication Python Client',
       author='Venkata Pingali',
       author_email='pingali@gmail.com',
       url='http://www.github.com/pingali/pyAadhaarAuth',
-      packages=['AadhaarAuth'],
+      packages=['AadhaarAuth', 'AadhaarAuth.lib'],
+      scripts=['bin/aadhaar-sample-client.py', 
+               'bin/aadhaar-batch-client.py',
+               'bin/aadhaar-generate-client.py'],
+      package_data={'AadhaarAuth.fixtures': ['*.cer', '*.cfg', 
+                                             '*.pem', '*.p12' '*.xml'],
+                    'AadhaarAuth.xsd': ['*.xsd']},
       license='LICENSE.txt',
       long_description=open('README.md').read(),
+      classifiers=classifiers, 
       requires=['lxml', 'config', 'M2Crypto', 'requests']
      )
 
